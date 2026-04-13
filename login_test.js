2cc4dcbc9e43f5e2379c681d9fa54464325d1e83
ben_caetano <benjamim.c13@gmail.com>
2026-04-13 19:50:51 -0300

    Primeiro Commit

---
+++ login_test.js
@@ -0,0 +1,44 @@
+Feature('login');
+
+Scenario('login com sucesso',  ({ I }) => {
+
+    I.amOnPage('http://automationpratice.com.br/');
+    I.click('Login')
+    I.waitForText('Login',10)
+    I.fillField('#user','abc@yahoo.com')
+    I.fillField('#password','123456')
+    I.click('#btnLogin')
+    I.waitForText('Login realizada',5)
+}).tag('@sucesso')
+
+Scenario('login apenas com e-mail',  ({ I }) => {
+
+    I.amOnPage('http://automationpratice.com.br/');
+    I.click('Login')
+    I.waitForText('Login',10)
+    I.fillField('#user','abc@yahoo.com')
+    I.click('#btnLogin')
+    I.waitForText('Senha inválida',5)
+
+})
+
+Scenario('login apenas com senha ',  ({ I }) => {
+
+    I.amOnPage('http://automationpratice.com.br/');
+    I.click('Login')
+    I.waitForText('Login',10)
+    I.fillField('#password','123456')
+    I.click('#btnLogin')
+    I.waitForText('E-mail inválido',5)
+
+})
+
+Scenario('login sem e-mail e senha',  ({ I }) => {
+
+    I.amOnPage('http://automationpratice.com.br/');
+    I.click('Login')
+    I.waitForText('Login',10)
+    I.click('#btnLogin')
+    I.waitForText('E-mail inválido',5)
+
+});
